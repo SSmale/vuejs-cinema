eaa9edb43ffc26e6e3f77a07275b50338392d325
SimonSmale <simon@smalemail.net>
2017-12-03 21:19:15 +0000

    completed init setup and started to build components

--- src/main.js
+++ src/main.js
@@ -1,1 +1,49 @@
 import './style.scss'
+import Vue from 'vue'
+import genres from './util/genres'
+
+new Vue({
+    el: '#app',
+    components: {
+        'movie-list': {
+            template: `<div id="movie-list">
+                            <div class="movie" v-for="movie in movies">{{ movie.title }}</div>
+                        </div>`,
+            data() {
+                return {
+                    movies: [{
+                            title: 'pulp fiction'
+                        },
+                        {
+                            title: 'Home Alone'
+                        },
+                        {
+                            title: 'Austin Powers'
+                        }
+                    ]
+                }
+            }
+        },
+        'movie-filter': {
+            data() {
+                return {
+                    genres
+                }
+            },
+            template: `<div id="movie-filter">
+                            <h2>Filter Results</h2>
+                            <div class="filter-group">
+                                <check-filter v-for="genre in genres" :name="genre"></check-filter>
+                            </div>
+                        </div>`,
+            components: {
+                'check-filter': {
+                    template: `<div class="check-filter">
+                    <span class="checkbox"></span>
+                    <span class="check-filter-title">{{ name }}</span></div>`,
+                    props: ['name']
+                }
+            }
+        }
+    }
+})
