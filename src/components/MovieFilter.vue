51a58d86e333297636a5c1831aaf7f2c4215073d
SimonSmale <simon@smalemail.net>
2017-12-04 11:47:35 +0000

    Added display for times filter

--- src/components/MovieFilter.vue
+++ src/components/MovieFilter.vue
@@ -1,6 +1,11 @@
 <template>
     <div id="movie-filter">
         <h2>Filter Results</h2>
+        <h3>By time of day</h3>
+        <div class="filter-group">
+            <check-filter v-for="time in times" :name="time" v-on:check-filter="checkFilter"></check-filter>
+        </div>
+        <h3>By genre</h3>
         <div class="filter-group">
             <check-filter v-for="genre in genres" :name="genre" v-on:check-filter="checkFilter"></check-filter>
         </div>
@@ -9,11 +14,14 @@
 
 <script>
 import genres from "../util/genres";
+import times from "../util/times";
+
 import CheckFilter from "./CheckFilter.vue";
 export default {
   data() {
     return {
-      genres
+      genres,
+      times
     };
   },
   methods: {
