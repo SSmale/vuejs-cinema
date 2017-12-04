716cae8510502b3ee2298c8e3425fd182a613bbc
SimonSmale <simon@smalemail.net>
2017-12-04 14:16:43 +0000

    details page populated with content

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="detail" v-if="movie">
-      <movie-item :movie="movie.movie"></movie-item>
+      <movie-item :movie="movie">
+          <div class="movie-detials">
+            <p class="movie-genre">{{ movie.Genre }}</p>
+            <p class="movie-plot">{{ movie.Plot }}</p>
+            <table>
+                <tr><td>Released Date</td><td>{{ movie.Released }}</td></tr>
+                <tr><td>Running Time</td><td>{{ movie.Runtime }}</td></tr>
+                <tr><td>Director</td><td>{{ movie.Director }}</td></tr>
+                <tr><td>Cast</td><td>{{ movie.Actors }}</td></tr>
+            </table>
+          </div>
+      </movie-item>
+      <div class="home">
+          <router-link :to="{ name: 'home' }">Back to results</router-link>
+      </div>
   </div>
 </template>
 
@@ -11,7 +25,7 @@ export default {
   computed: {
     movie() {
       let movie = this.movies.find(movie => movie.id === this.$route.params.id);
-      return movie ? movie : null;
+      return movie ? movie.movie : null;
     }
   },
   components: {
