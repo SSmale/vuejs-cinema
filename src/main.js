4101421a1fdc21b365636af2d20e5ff806bae563
SimonSmale <simon@smalemail.net>
2017-12-04 12:54:05 +0000

    added comments to main.js and init global bus

--- src/main.js
+++ src/main.js
@@ -1,18 +1,34 @@
 import './style.scss'
 
+// Import Vue.use components
 import Vue from 'vue'
 import VueResource from 'vue-resource'
+
+// Import 3rd Party Libaries
 import moment from 'moment-timezone'
 
+// Import Components
 import MovieList from "./components/MovieList.vue";
 import MovieFilter from "./components/MovieFilter.vue";
 
+// Define bus for global props passing... VueX?
+const bus = new Vue();
+
+// Vue.use...
 Vue.use(VueResource)
+
+// Adding objects to the global namespace
 Object.defineProperty(Vue.prototype, '$moment', {
     get() {
         return this.$root.moment
     }
 })
+Object.defineProperty(Vue.prototype, '$bus', {
+    get() {
+        return this.$root.bus
+    }
+})
+
 moment.tz.setDefault("UTC")
 
 new Vue({
@@ -38,7 +54,8 @@ new Vue({
         genre: [],
         time: [],
         movies: [],
-        date: moment()
+        date: moment(),
+        bus
     },
     created() {
         // GET /api
